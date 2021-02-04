36ce709105c53d719b912200034cac7848e0fe6f
Jo Vincent Ruiz <jovincent.ruiz@gmail.com>
2021-02-04 14:34:24 -0800

    fixed error

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 
 data = pd.read_csv('./housingdata_test.csv').to_json(orient='records')
+
 url = 'http://127.0.0.1:8000/predict/rf'
 
 r = requests.post(url, json=data)
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ def pre_processing_pipeline(data):
     pre-processing pipeline for incoming data
     '''
     # removing these columns as they were deemed statistically insiginificant by linear regression analysis
-    remove_columns = ['CRIM', 'INDUS', 'NOX', 'AGE', 'RAD', 'TAX']
+    remove_columns = ['CRIM', 'INDUS', 'NOX', 'AGE', 'RAD']
     
     # drop the rows with null values from the dataset
     data = data.dropna()
