668899ae63215af5de514b72c846de3c8fd5224c
Jo Vincent Ruiz <jovincent.ruiz@gmail.com>
2021-02-04 15:08:28 -0800

    added comments to inference.py

--- inference.py
+++ inference.py
@@ -2,10 +2,13 @@ import requests
 import pandas as pd
 import json
 
+# load csv and convert to list of json objects
 data = pd.read_csv('./housingdata_test.csv').to_json(orient='records')
 
+# setup url
 url = 'http://127.0.0.1:8000/predict/linear'
 
+# make request and print response
 r = requests.post(url, json=data)
 content = json.loads(r.content) 
 print(content)
